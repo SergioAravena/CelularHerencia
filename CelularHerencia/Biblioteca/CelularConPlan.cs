633a9568c8c223a371da19ca4d186771108bd295
ShadowBlack <sergio.guetta17@gmail.com>
2017-03-05 21:18:46 -0300

    Primero  Cambios Agregando Variables

---
+++ CelularHerencia/Biblioteca/CelularConPlan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca
+{
+  public  abstract class CelularConPlan
+    {
+
+        private int numero;
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        private string titular;
+
+        public string Titular
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+
+    }
+}
---
+++ CelularHerencia/Celular/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Celular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
