a04c6ef8466c947f8e6c5a7243c27cc48f3db1cb
ShadowBlack <sergio.guetta17@gmail.com>
2017-03-05 23:11:12 -0300

    Ultima Clase termino del proyecto

---
+++ CelularHerencia/Biblioteca/CelularCtaIlimitada.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class CelularCtaIlimitada : CelularConPlan
+    {
+        private const double VALOR_SMS = 100;
+
+        private const int VALOR_ADICIONAL = 120;
+
+
+
+        public override double CalcularPagoMes()
+        {
+            double monto = ValorPlan + (CantidadSMS + VALOR_SMS);
+
+            int minAdicional = MinutosUtilizados - MinutosPlan;
+
+            if (minAdicional>0)
+            {
+                return monto + minAdicional + VALOR_ADICIONAL;
+            }
+            else
+            {
+                return monto;
+            }
+
+        }
+
+        public override void ImprimirFactura()
+        {
+            Console.WriteLine("Numero Celular "+Numero);
+            Console.WriteLine("Tipo Cuenta : Ilimitada ");
+            Console.WriteLine("Minutos plan "+MinutosPlan);
+            Console.WriteLine("Monto a pagar "+CalcularPagoMes());
+
+        }
+    }
+}
